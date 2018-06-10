fa6ae2697e7802a74b5fad0fcc473b209e36adbd
Your Name <gatleekaw@gmail.com>
2018-06-10 16:08:04 +1000

    Added user input to change and proper refreshing of colors

--- color.py
+++ color.py
@@ -4,49 +4,87 @@ import os
 
 
 
-os.system("./colorpane.sh")
 
 
-DIRECTORY = "."
+DIRECTORY = "./"
 CONFIG_NAME = "config"
 POLYBAR_BAR = "autumn"
 
-def refreshPolybar():
-    os.system("cp colors %s/config" % (DIRECTORY))
-    os.system("cat %s/configbody >> %s/config" % (DIRECTORY, DIRECTORY))
+def refreshPolybar(array):
+    with open("%scolors" % (DIRECTORY), "w") as writefile:
+        for i in array:
+            writefile.write(i)
+
+    os.system("cp %scolors %sconfig" % (DIRECTORY, DIRECTORY))
+    os.system("cat %sconfigbody >> %sconfig" % (DIRECTORY, DIRECTORY))
     os.system("killall polybar")
     os.system("nohup polybar autumn &")
+    subprocess.call(["nohup", "polybar", "autumn", "&"], shell=False, stdout = open('/dev/null', "w"))
+    os.system("clear")
+    os.system("./colorpane.sh")
 
 
-def readColors():
-    with open("%s/colors" % (DIRECTORY), "r") as readfile:
-        for line in readfile:
-            print(line)
+
 
 
 
 #Takes string and modifies color
-def modifyLine():
-    newfile = open('%s/newfile' % (DIRECTORY), 'w') 
-    with open("%s/colors" % (DIRECTORY), "r") as readfile:
-        for line in readfile:
-            print((line.split("="))[0])
-            newfile.write(changeColor(line))
-    newfile.close
+def getVarName(rawLine):
+    return (rawLine.split("="))[0]
 
 
-def changeColor(line, number=0):
+def getCurrent(rawLine):
+    line = rawLine
     i=0
     while (i < len(line)):
         if line[i:].startswith('xrdb:color'):
-            return line[:i+10] + str(number) + line[i+11:]
+            return line[i+10]
         i+=1
+
+
+
+def main():
+    os.system("./colorpane.sh")
+    colorArray = genColorArray()
+    start = 0 
+    for i in range(len(colorArray)):
+        if start == 1:
+            colorIndex = input("What colour would you like %s to be? Currently: %s" \
+                    % (getVarName(colorArray[i]), getCurrent(colorArray[i])   ))
+            modifyArray(colorArray, i, colorIndex)
+            refreshPolybar(colorArray)
+
+        elif colorArray[i].startswith("[colors]"):
+            start = 1
+        else:
+            pass
+
+
+
+def modifyArray(array, index, value):
+    line = array[index] 
+    if (value == ""):
+        return
+    else:
+        i = 0
+        while (i < len(line)):
+            if line[i:].startswith('xrdb:color'):
+                line = line[:i+10] + str(value) + line[i+11:]
+            i+=1
+        array[index] = line 
+
+            
+def genColorArray():
+    outputArray = []
     
-    return line
+    with open("%scolors" % (DIRECTORY), "r") as readfile:
+        for line in readfile:
+            outputArray.append(line)
+    return outputArray
+     
     
         
 
-modifyLine()
-    
+main()    
 
 
